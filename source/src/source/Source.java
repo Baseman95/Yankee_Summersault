d4ff5874c6a5d7c51dde27674436dbb988bab3de
Link162534 <link162534@live.at>
2015-10-17 16:54:52 +0200

    - Created Project.

---
+++ source/src/source/Source.java
@@ -0,0 +1,16 @@
+package source;
+
+/**
+ *
+ * @author Link162534
+ */
+public class Source {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("DATS THE SHIT!");
+    }
+    
+}
