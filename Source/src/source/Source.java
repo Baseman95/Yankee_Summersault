f1c1357cfa2f343b3d59b47000e301223e4b7468
Roman Briebauer <roman_briebauer@hotmail.com>
2015-10-17 17:59:13 +0200

    + JOSEPH

--- Source/src/source/Source.java
+++ Source/src/source/Source.java
@@ -15,7 +15,7 @@ public class Source {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HALLO ICH BIN PETER > Joseph");
+        System.out.println("HALLO ICH BIN PETER > eph");
     }
     
 }
