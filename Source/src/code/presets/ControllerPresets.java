dcc9358d4d5c2fdbc967bb88a0dc1fd5dfac5b7a
Baseman <Eris@10.0.0.17>
2016-02-18 04:24:55 +0100

    Merge

--- Source/src/code/presets/ControllerPresets.java
+++ Source/src/code/presets/ControllerPresets.java
@@ -26,23 +26,22 @@ public class ControllerPresets {
         ControllerInterface moveTo = (GameObject gameObject, long tick, World world, KeyManager manager) -> {
             DataObject data = (DataObject) gameObject.getData();
 
-        int velocity = 3; //projektilgeschwindigkeit in px per sec           
-        PolarVector pv = new PolarVector(data.getPositionData().getRotation(), velocity);
-        data.getMovementData().setMovementX(PolarVector.xFromPolar(pv));
-        data.getMovementData().setMovementY(PolarVector.yFromPolar(pv));
-    };
+            PolarVector pv = new PolarVector(data.getPositionData().getRotation(), velocity);
+            data.getMovementData().setMovementX(PolarVector.xFromPolar(pv));
+            data.getMovementData().setMovementY(PolarVector.yFromPolar(pv));
+        };
+        return moveTo;
+    }*/
 
     public static ControllerInterface PLAYER = (GameObject gameObject, long tick, World w, KeyManager manager) -> {
         Chassis c = ((Chassis) gameObject);
         Drive d = c.getDrive();
+        Weapon w0 = c.getWeapons().get(0);
         d.setAccelerate(manager.isKeyPressed(KeyEvent.VK_W));
         d.setDecelerate(manager.isKeyPressed(KeyEvent.VK_S));
         d.setBreaks(manager.isKeyPressed(KeyEvent.VK_SPACE));
         d.setTurnLeft(manager.isKeyPressed(KeyEvent.VK_A));
         d.setTurnRight(manager.isKeyPressed(KeyEvent.VK_D));
-        if (c.getWeapons().get(0) != null) {
-            Weapon w0 = c.getWeapons().get(0);
-            w0.setShoot(manager.isKeyPressed(KeyEvent.VK_1));
-        }
+        w0.setShoot(manager.isKeyPressed(KeyEvent.VK_1));
     };
 }
